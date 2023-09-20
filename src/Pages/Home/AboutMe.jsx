79108f3a3b7e13d549d28fb150609659998e2efb
sharad-113 <sharadmishra113@gmail.com>
2023-09-20 12:42:27 +0530

    Adding the project

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,34 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <p>Image Appearing on left is Dummy</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            An MCA final-year student looking for internships or job
+            opportunities in Web Development or software development. An
+            organized and dependable candidate who is successful at managing
+            multiple priorities with a positive attitude Willingness to take on
+            added responsibilities to meet team goals. Hardworking and
+            passionate job seeker with strong organizational skills eager to
+            secure an entry-level software developer position. Ready to help the
+            team achieve company goals. Enthusiastic post-graduate with basic
+            knowledge of coding and design.
+          </p>
+          <p className="hero--section-description">
+            Proficient in C++, HTML5, CSS, REACT, and MongoDB
+            JavaScript,NodeJs,ExpressJs, EJS, Core Java, and MERN STACK. Ability
+            to learn new software and technologies quickly and have great
+            problem-solving skills. Ability to work in teams by providing
+            valuable support.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,34 @@
+const HeroSection = () => {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey,I am Sharad Kumar Mishra</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack MERN</span>
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            MCA final-year student seeking web or software development
+            internships or an entry-level positions.
+            <br />
+            Proficient in C++, HTML5, CSS, REACT, and MongoDB
+            JavaScript,NodeJs,ExpressJs, EJS, Core Java, and MERN STACK
+          </p>
+        </div>
+        <br />
+        <a
+          href="https://www.linkedin.com/in/sharad-kumar-mishra-9502641b2/"
+          style={{ textDecoration: "none" }}
+        >
+          <button className="btn btn-primary">My Linkedin Profile</button>
+        </a>
+      </div>
+      <div className="hero--section--img">
+        <img src="../../src/assets/Optimized-Coding.jpg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+};
+export default HeroSection;
